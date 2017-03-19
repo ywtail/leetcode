f92a744f88834a108bf4f61430ce6a232900045e
ywtail <ywtail@gmail.com>
2017-03-19 17:34:13 +0800

    17.3.19 @easy; Sqrt(x)

---
+++ 50_69.py
@@ -0,0 +1,10 @@
+# 69. Sqrt(x)
+# 42ms beats 98.44%
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(math.sqrt(x))
