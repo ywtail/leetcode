ac53dd5248ac39923f46368ae3bbdc4f4530e7f2
ywtail <ywtail@gmail.com>
2017-06-21 21:04:34 +0800

    17.6.21 @easy; 63_367. Valid Perfect Square; 二分法left right

---
+++ 63_367_2.py
@@ -0,0 +1,44 @@
+# coding:utf-8
+# 367. Valid Perfect Square 有效的完美平方
+# 49ms beats 32.10%
+
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        left = 1
+        right = num
+        while left <= right:
+            mid = (left + right) / 2
+            if mid * mid == num:
+                return True
+            elif mid * mid < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+
+
+solution = Solution()
+print solution.isPerfectSquare(16)
+# 4
+print solution.isPerfectSquare(8)
+# 21
+
+'''
+题目：
+给定一个正整数num，写一个函数返回True，如果num是一个完美的平方，否则返回False。
+注意：不要使用任何内置的库函数，如sqrt。
+示例1：
+输入：16
+返回值：True
+示例2：
+输入：14
+返回：False
+
+分析：
+二分法，时间复杂度 O(logn)
+使用二分法找 sqrt(n)，每次搜索范围减小一半。
+'''
